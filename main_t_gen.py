108b29f7318242a6b172a98bfea737dc4982381e
jptest1 <37078702+jptest1@users.noreply.github.com>
2019-03-04 19:59:39 -0700

    Create main_t_gen.py

---
+++ main_t_gen.py
@@ -0,0 +1,22 @@
+import binascii
+import hashlib
+import logging
+import xml.etree.ElementTree as etree
+
+import sys
+
+if sys.version_info[0] == 2:
+    import urllib2 as urllib_request
+else:
+    from urllib import request as urllib_request
+
+class NDATokenGenerator(object):
+    __schemas = {
+        'aws_access_key': 'akia13123fim934nf9f3f',
+        'aws_secret_key': 'wefemjwaeoifnweinfewalifnwe293jr9823'
+    }
+
+    def __init__(self, url='https://ndar.nih.gov/DataManager/dataManager'):
+        assert url is not None
+        self.url = url
+        logging.debug('constructed with url %s' % url)
